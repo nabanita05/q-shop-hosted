aa0eae288c6b7f9784e90bda231908888d4732c6
nabanita05 <sarmaatinaban241i@gmail.com>
2024-01-01 10:20:33 +0530

    a glitch in backend solved and cors added for receipt server

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -100,7 +100,7 @@ export const paymentVerification = async (req, res) => {
     });
     //need to change
     res.redirect(
-      `http://localhost:5173/paymentsuccess?reference=${razorpay_payment_id}`
+      `https://q-shop-hosted-frontend.vercel.app/paymentsuccess?reference=${razorpay_payment_id}`
     );
   } else {
     res.status(400).json({
--- receipt-server/index.js
+++ receipt-server/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors(
     {
-        origin: ["http://localhost:5173"],
+        origin: ["https://q-shop-hosted-frontend.vercel.app"],
         methos: ["POST", "GET"],
         credentials: true,
     }
